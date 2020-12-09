b82de69440fa563a14d09e6f114ace275d7061d8
Kader Gür <kadergur236@gmail.com>
2020-12-09 21:23:30 +0300

    All Project

---
+++ project 2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace project_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota:  " + comboBox1.Text + "-" + comboBox2.Text + " Tarih:  " + dateTimePicker1.Text + "   " + "Saat:  " + maskedTextBox1.Text + "Ad-Soyad:  " + textBox1.Text + "TC:  " + maskedTextBox2.Text + "   " + "Telefon:  " + maskedTextBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label9.Text = comboBox1.Text;
+            comboBox1.Text = comboBox2.Text;
+            comboBox2.Text = label9.Text;
+        }
+    }
+}
